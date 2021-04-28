f3f8634992a342c2884633fea7e5edd2f992be3c
ByronSuarez <guzman.byron96@gmail.com>
2021-04-28 16:16:26 -0500

    modificaciones

---
+++ MVCCrudApi/Models/Validations/PeopleValidate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrudApi.Models
+{
+
+    [MetadataType(typeof(people.MetaData))]
+    public partial class people
+    {
+        //clases selladas
+        sealed class MetaData
+        {
+            [Display(Name = "Nombre")] //texto q se muestra en los mensajes
+            [Required]
+            [StringLength(5)]
+            public string name;
+        }
+    }
+}
